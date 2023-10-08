8dc8e859ccb5250b12be293386e6422d8d04442e
gscaep7329 <49217718+gscaep7329@users.noreply.github.com>
2023-10-08 17:12:40 +0800

    Update combvec.m

--- combvec.m
+++ combvec.m
@@ -1,5 +1,13 @@
-function out = my_combvec(A,B)
+function out = combvec(A,varargin)
 % Generate all combinations of input arrays
-out = [repmat(A,1, numel(B));
+npar = numel(varargin);
+if npar == 0
+    out = A;
+elseif npar > 1 % recursive
+    out = combvec(combvec(A,varargin{1}),varargin{2:end});
+else
+    B = varargin{1};
+    out = [repmat(A,1, numel(B));
        reshape(repmat(B,size(A,2),1), 1, size(A,2)*numel(B))];
+end
    
